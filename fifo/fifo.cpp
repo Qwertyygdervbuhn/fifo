f3e35cb56c34cfc773c6781ac4115258143db380
Alexander Dm. <dabl21291@gmail.com>
2024-11-20 19:25:44 +0300

    Добавьте файлы проекта.

---
+++ fifo/fifo.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+using namespace std;
+
+template <typename T>
+class Queue {
+private:
+    struct Node {
+        T data;
+        Node* next;
+    };
+
+    Node* front;
+    Node* rear;
+
+public:
+    Queue() {
+        front = nullptr;
+        rear = nullptr;
+    }
+
+    ~Queue() {
+        while (front != nullptr) {
+            dequeue();
+        }
+    }
+
+    void enqueue(T value) {
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = nullptr;
+        if (rear == nullptr) {
+            front = newNode;
+            rear = newNode;
+        }
+        else {
+            rear->next = newNode;
+            rear = newNode;
+        }
+    }
+
+    void dequeue() {
+        if (front == nullptr) {
+            cout << "Queue is empty!" << endl;
+            return;
+        }
+        Node* temp = front;
+        front = front->next;
+        if (front == nullptr) {
+            rear = nullptr;
+        }
+        delete temp;
+    }
+
+    T peek() {
+        if (front == nullptr) {
+            cout << "Queue is empty!" << endl;
+            return T();
+        }
+        return front->data;
+    }
+
+    bool isEmpty() {
+        return front == nullptr;
+    }
+};
+
+int main() {
+    Queue<int> queue;
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    cout << "Front element: " << queue.peek() << endl;
+    queue.dequeue();
+    cout << "Front element after dequeue: " << queue.peek() << endl;
+}
